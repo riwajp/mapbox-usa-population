fccc0512f550af24d8805542bd03bd29a2b4ce93
riwajp <riwajfictious@gmail.com>
2022-05-29 14:03:08 +0545

    toggle buttons, idle event

--- src/App.js
+++ src/App.js
@@ -1,70 +1,82 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 
 import "./App.css";
-
 import Map from "./components/Map";
 import population from "./population.json";
 import literacy from "./literacy.json";
+
+//App
 function App() {
-  //prepare data
-  let data = { population: {}, literacy: {} };
+  //prepare data====================
+  let data = { population2022: {}, literacy: {}, population2019: {} };
+
   for (let i of population) {
-    data.population[i.State] = parseInt(i["2,022"].replace(/,/g, ""));
+    data.population2022[i.State] = parseInt(i["2,022"].replace(/,/g, ""));
+    data.population2019[i.State] =
+      parseInt(i["2,019"].replace(/,/g, "")) + Math.random() * 10000000; //random data
   }
   for (let i of literacy) {
     data.literacy[i.State] = parseFloat(i.literacyRate);
   }
 
+  //color scales====================
+  const population_color_scale = [
+    0,
+    "black",
+    500000,
+    "#4fd685",
+    750000,
+    "#9cd64f",
+    1000000,
+    "#d6d64f",
+    2500000,
+    "#d6b04f",
+    5000000,
+    "#d68c4f",
+    7500000,
+    "#d66a4f",
+    10000000,
+    "#8B4225",
+    25000000,
+    "#d6524f",
+  ];
+  const literacy_color_scale = [
+    0,
+    "black",
+    0.5,
+    "#4fd685",
+    0.65,
+    "#9cd64f",
+    0.7,
+    "#d6d64f",
+    0.75,
+    "#d6b04f",
+    0.78,
+    "#d68c4f",
+    0.8,
+    "#d66a4f",
+    0.83,
+    "#8B4225",
+    0.85,
+    "#d6524f",
+    0.9,
+    "red",
+  ];
   const color_scales = {
-    population: [
-      0,
-      "black",
-      500000,
-      "#4fd685",
-      750000,
-      "#9cd64f",
-      1000000,
-      "#d6d64f",
-      2500000,
-      "#d6b04f",
-      5000000,
-      "#d68c4f",
-      7500000,
-      "#d66a4f",
-      10000000,
-      "#8B4225",
-      25000000,
-      "#d6524f",
-    ],
-    literacy: [
-      0,
-      "black",
-      0.5,
-      "#4fd685",
-      0.65,
-      "#9cd64f",
-      0.7,
-      "#d6d64f",
-      0.75,
-      "#d6b04f",
-      0.78,
-      "#d68c4f",
-      0.8,
-      "#d66a4f",
-      0.83,
-      "#8B4225",
-      0.85,
-      "#d6524f",
-      0.9,
-      "red",
-    ],
+    population2022: population_color_scale,
+    population2019: population_color_scale,
+    literacy: literacy_color_scale,
   };
+
+  //return====================
   return (
     <div className="App">
       <Map
         color_scales={color_scales}
         data={data}
         default_active_data="literacy"
+        center={[-107.484229, 43.644026]}
+        zoom={3}
       />
     </div>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,37 +1,35 @@
 import React, { useState, useEffect, useRef } from "react";
-
 import mapboxgl from "mapbox-gl";
 import states from "../states.geojson";
-
 import Legend from "./Legend";
 
-function Map({ color_scales, data, default_active_data }) {
-  console.log("MAP");
+function Map({ color_scales, data, default_active_data, center, zoom }) {
   const [layer_state, setLayerState] = useState({
     active_data: default_active_data,
     color_scale: color_scales[default_active_data],
   });
+  const [features, setFeatures] = useState([]);
+
+  //map initialization====================
   mapboxgl.accessToken =
     "pk.eyJ1Ijoicml3YWpwIiwiYSI6ImNreGhqdmNrcTJheXUyeHRoZGV4Mm9qZTAifQ.krIdQfzikO4kh6g3j6ClLg";
   const map_container = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
 
   const data_keys = Object.keys(data);
-  //render the map
+
   useEffect(() => {
+    //render the map====================
     if (map.current) return;
     map.current = new mapboxgl.Map({
       container: map_container.current,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
+
+      zoom: 1,
     });
 
     map.current.on("load", () => {
-      //add sources and layers
+      //add sources and layers====================
       map.current.addSource("states", { type: "geojson", data: states });
 
       map.current.addLayer({
@@ -57,66 +55,72 @@ function Map({ color_scales, data, default_active_data }) {
         source: "states",
         paint: { "line-color": "black", "line-width": 2 },
       });
-    });
-    map.current.on("render", () => {
-      const states_layer = map.current.getLayer("states_layer");
 
-      if (states_layer) {
-        const states_layer = map.current.getLayer("states_layer");
+      map.current.once("idle", () => {
+        const features = map.current.queryRenderedFeatures({
+          layers: ["states_layer"],
+        });
+        setFeatures(features);
 
-        if (states_layer) {
-          const features = map.current.queryRenderedFeatures({
-            layers: ["states_layer"],
-          });
-
-          for (let feature of features) {
-            let feature_data_state = {};
-            for (let i of data_keys) {
-              feature_data_state[i] = data[i][feature.properties.name];
-            }
-            map.current.setFeatureState(
-              { source: "states", id: feature.id },
-              {
-                ...feature_data_state,
-                ...layer_state,
-              }
-            );
-          }
-        }
-      }
+        map.current.flyTo({
+          center: [center[0], center[1]],
+          zoom: zoom,
+          speed: 5,
+        });
+      });
     });
   });
+
+  useEffect(() => {
+    for (let feature of features) {
+      let feature_state = {};
+      for (let i of data_keys) {
+        feature_state[i] = data[i][feature.properties.name];
+      }
+      map.current.setFeatureState(
+        { source: "states", id: feature.id },
+        {
+          ...feature_state,
+        }
+      );
+    }
+  }, [features]);
+
+  useEffect(() => {
+    const states_layer = map.current.getLayer("states_layer");
+    if (states_layer) {
+      //if states_layer rendered====================
+      map.current.setPaintProperty("states_layer", "fill-color", [
+        "interpolate",
+        ["linear"],
+
+        ["coalesce", ["feature-state", layer_state.active_data], 0],
+        ...layer_state.color_scale,
+      ]);
+    }
+  }, [layer_state]);
 
   return (
     <div>
       <Legend layer_state={layer_state} />
+
       <div ref={map_container} className="map-container"></div>
 
-      <button
-        onClick={() => {
-          const temp_new_state =
-            layer_state.active_data == data_keys[0]
-              ? {
-                  active_data: data_keys[1],
-                  color_scale: color_scales[data_keys[1]],
-                }
-              : {
-                  active_data: data_keys[0],
-                  color_scale: color_scales[data_keys[0]],
-                };
-          setLayerState(temp_new_state);
+      {data_keys.map((k) => (
+        <button
+          key={k}
+          onClick={() => {
+            const temp_new_state = {
+              active_data: k,
+              color_scale: color_scales[k],
+            };
 
-          map.current.setPaintProperty("states_layer", "fill-color", [
-            "interpolate",
-            ["linear"],
-
-            ["coalesce", ["feature-state", temp_new_state.active_data], 0],
-            ...temp_new_state.color_scale,
-          ]);
-        }}
-      >
-        Toggle
-      </button>
+            setLayerState(temp_new_state);
+          }}
+        >
+          {k}
+        </button>
+      ))}
     </div>
   );
 }
